0eaad974288d07795c6876cbc6b065173953fed7
James Lanham <james.r.lanham2021@gmail.com>
2025-02-11 15:18:31 -0500

    Adding additional fields to book struct

--- Backend/internal/library/models/book.go
+++ Backend/internal/library/models/book.go
@@ -1,12 +1,18 @@
 // Package models stores all of our models for the library module
 package models
 
-import "Home-Intranet-v2-Backend/internal/platform/repository"
+import (
+	"Home-Intranet-v2-Backend/internal/platform/repository"
+	"time"
+)
 
 // Book is the type for books in our library
 type Book struct {
-	repository.Model `bson:",inline"`
-	Title            string `bson:"title"`
-	AuthorFirstName  string `bson:"author_first_name"`
-	AuthorLastName   string `bson:"author_last_name"`
+	repository.Model `bson:",inline" json:",inline"`
+	Title            string    `bson:"title" json:"title"`
+	Authors          []Author  `bson:"authors" json:"authors"`
+	Shelf            string    `bson:"shelf" json:"shelf"`
+	CheckedOut       bool      `bson:"checked_out" json:"checked_out"`
+	CheckedOutBy     string    `bson:"checked_out_by" json:"checked_out_by"`
+	CheckedOutTime   time.Time `bson:"checked_out_time" json:"checked_out_time"`
 }
